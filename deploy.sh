24de5b50f7b6da12bc0319e0be0f836d89bfb779
brian-ms <56346103+brian-ms@users.noreply.github.com>
2024-08-14 00:09:15 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /home/imperium/lc.imperiumgroup.co.ke/public_html
+
+# Pull the latest changes from GitHub
+git pull origin main
+
+# Rebuild Docker containers
+docker-compose down
+docker-compose up -d --build
+
+# Run database migrations
+docker-compose run web python manage.py migrate
+
+# Collect static files (if needed)
+docker-compose run web python manage.py collectstatic --noinput
+
+# Restart the application (optional, depending on your setup)
+docker-compose restart
